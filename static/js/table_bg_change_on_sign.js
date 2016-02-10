01343e5dd52d5dc2601a0068b328e9e9bcb513a8
Visipuki <visipuki@mail.ru>
2016-02-10 14:02:20 +0300

    bg change at negative balance

--- static/js/table_bg_change_on_sign.js
+++ static/js/table_bg_change_on_sign.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
-	$("td").each(function() {
+	$("#accounts td").each(function() {
 		var value = parseInt($(this).html());
+		if (value>0&&value<1000) {
+			$(this).addClass("bg-warning");
+		}
 		if (value<0) {
-			$(this).addClass("attention");
+			$(this).addClass("bg-danger");
 		}
 	});
 });
